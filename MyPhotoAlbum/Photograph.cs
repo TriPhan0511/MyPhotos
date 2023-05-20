bd9c29b988097ff1251b7e70b4d532ba927097a0
Tri Phan <phanphutri@gmail.com>
2023-05-20 17:11:37 +0700

    Updated

--- MyPhotoAlbum/Photograph.cs
+++ MyPhotoAlbum/Photograph.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TriPhan.MyPhotoAlbum
 {
+    /// <summary>
+    /// The Photograph class represents a
+    /// photographic image stored in the 
+    /// file system.
+    /// </summary>
     public class Photograph
     {
+        private string _fileName;
+        private Bitmap _bitmap;
+
+        public Photograph(string fileName)
+        {
+            _fileName = fileName;
+            _bitmap = null;
+        }
+
+        public string FileName
+        {
+            get { return _fileName; }
+        }
+
+        public Bitmap Image
+        {
+            get
+            {
+                if (_bitmap == null)
+                {
+                    _bitmap = new Bitmap(_fileName);
+                }
+                return _bitmap;
+            }
+        }
     }
 }
