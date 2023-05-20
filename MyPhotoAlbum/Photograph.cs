96810d24d04f006d8bd52f9a611a4cc4c801a5f9
Tri Phan <phanphutri@gmail.com>
2023-05-20 16:43:03 +0700

    Updated

--- MyPhotoAlbum/Photograph.cs
+++ MyPhotoAlbum/Photograph.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyPhotoAlbum
+namespace TriPhan.MyPhotoAlbum
 {
-    public class Class1
+    public class Photograph
     {
     }
 }
